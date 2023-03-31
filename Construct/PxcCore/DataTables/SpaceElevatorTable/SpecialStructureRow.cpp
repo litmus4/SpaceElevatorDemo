3090adc0337bf618fd8458dfbdf6975b3bf63a8b
litmus4 <scrudlds@163.com>
2023-03-31 20:02:47 +0800

    优化SpecialStructureRow

--- Construct/PxcCore/DataTables/SpaceElevatorTable/SpecialStructureRow.cpp
+++ Construct/PxcCore/DataTables/SpaceElevatorTable/SpecialStructureRow.cpp
@@ -14,9 +14,10 @@ void CSpecialStructureRow::Read(PxcUtil::CCSVTableOperator& tabop)
 {
 	tabop.GetValue("ID", m_iID);
 	tabop.GetValue("DisplayText", m_iDisplayTextID);
-	tabop.GetValue("AssetName", m_strAssetName);
+	tabop.GetValue("AssetPath", m_strAssetFile);
 	tabop.GetValue("ZLocation", m_dZLocation);
 	tabop.GetValue("Height", m_dHeight);
 	tabop.GetValue("Offset", m_dOffset);
 	tabop.GetValue("EmbedType", *(int*)&m_eEmbedType);
+	tabop.GetValue("Docking", m_dDocking);
 }
--- Construct/PxcCore/DataTables/SpaceElevatorTable/SpecialStructureRow.h
+++ Construct/PxcCore/DataTables/SpaceElevatorTable/SpecialStructureRow.h
@@ -11,9 +11,10 @@ public:
 
 	int m_iID;
 	int m_iDisplayTextID;
-	std::string m_strAssetName;
+	std::string m_strAssetFile;
 	double m_dZLocation;//米（10单位）
 	double m_dHeight;//单位（0.1米）
 	double m_dOffset;//-1到1
 	SpaceElevatorDef::EEmbedType m_eEmbedType;//嵌入类型
+	double m_dDocking;//单位，0对应ZLocation
 };
